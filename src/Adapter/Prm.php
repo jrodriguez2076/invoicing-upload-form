07c2a20b38347b33575efd153042f3c41f7e402a
Roberto Briones <contacto@robertobriones.com>
2018-07-03 21:00:35 -0500

    Added Adapter to communicate with PRM and process the Entity.

---
+++ src/Adapter/Prm.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Adapter;
+
+use App\Entity\SellerRequest;
+use App\Exception\PrmException;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+
+class Prm
+{
+    /** @var ClientInterface */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $clientId;
+
+    /**
+     * @var string
+     */
+    protected $clientSecret;
+
+    public function __construct(ClientInterface $client, string $clientId, string $clientSecret)
+    {
+        $this->client = $client;
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    public function createAccount(SellerRequest $sellerRequest)
+    {
+        $requestBody = $this->createRequest($sellerRequest);
+
+        try {
+            $this->client->request(
+                'POST',
+                '/api/rest/latest/accounts',
+                [
+                    'headers' => $this->getWsseHeaders(),
+                    'json' => $requestBody,
+                ]
+            );
+        } catch (ClientException $exception) {
+            $responseBody = json_decode((string) $exception->getResponse()->getBody());
+
+            throw new PrmException($responseBody['message']);
+        } catch (ServerException $exception) {
+            $responseBody = json_decode((string) $exception->getResponse()->getBody());
+
+            throw new PrmException((string) $responseBody['message'], $responseBody);
+        }
+    }
+
+    protected function getWsseHeaders(): array
+    {
+        $nonce = uniqid();
+        $createdAt = date('c');
+        $wsse = sprintf(
+            'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"',
+            $this->clientId,
+            base64_encode(sha1(base64_decode($nonce) . $createdAt . $this->clientSecret, true)),
+            $nonce,
+            $createdAt
+        );
+
+        return [
+            'Authorization' => 'WSSE profile="UsernameToken"',
+            'X-WSSE' => $wsse,
+        ];
+    }
+
+    public function createRequest(SellerRequest $sellerRequest)
+    {
+        return [
+            'account' => [
+                'name' => $sellerRequest->getAccountName(),
+                'phone' => $sellerRequest->getPhoneNumber(),
+                'emailAddress' => $sellerRequest->getEmail(),
+                'countryId' => 'ec',
+                'owner' => 1,
+            ],
+        ];
+    }
+}
--- src/Controller/SellerRequestController.php
+++ src/Controller/SellerRequestController.php
@@ -4,21 +4,41 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Adapter\Prm;
 use App\Entity\SellerRequest;
 use App\Form\SellerRequestType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 
 class SellerRequestController extends AbstractController
 {
-    public function index(): Response
+    /**
+     * @var Prm
+     */
+    protected $prmAdapter;
+
+    public function __construct(Prm $prmAdapter)
+    {
+        $this->prmAdapter = $prmAdapter;
+    }
+
+    public function index(Request $request): Response
     {
         $sellerRequest = new SellerRequest();
         $form = $this->createForm(SellerRequestType::class, $sellerRequest);
 
-        return $this->render('sellerRequest.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->prmAdapter->createAccount($sellerRequest);
+        }
+
+        return $this->render(
+            'sellerRequest.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 }

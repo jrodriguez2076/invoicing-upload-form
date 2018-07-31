727a5695fff104eda591bb4c6801079b866464de
Miguel <miguel.ortiz.esp@gmail.com>
2018-07-31 15:53:31 -0500

    [DEVOPS-1277] Modified config.rb file for new install.

--- deploy/config.rb
+++ deploy/config.rb
@@ -1,13 +1,6 @@
-set :application, 'seller-signup-form'
 set :web_path, "public"
 set :php_fpm_service, 'php7.1-fpm'
 
 set :format_options, log_file: 'var/logs/capistrano.log'
 
-append :linked_files, '.env'
-
-append :copy_files, 'node_modules'
-
-after 'deploy:updated', 'linio:yarn:install'
-before 'deploy:symlink:release', 'linio:doctrine:migrate'
 after 'deploy:cleanup', 'linio:php:fpm:restart'

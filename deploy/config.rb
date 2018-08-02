1d0a63accc763e2d91462d370212643619a98075
Harrison Heck <harrison.heck@gmail.com>
2018-08-02 15:03:36 -0400

    ci: Fixed deploy due to missing env var and parameter file.
    We no longer use parameters.yml, and capistrano expects it by default. Also, the app requires
    APP_ENV for it to work correctly. This is missing when running composer.

--- deploy/config.rb
+++ deploy/config.rb
@@ -3,4 +3,10 @@ set :php_fpm_service, 'php7.1-fpm'
 
 set :format_options, log_file: 'var/log/capistrano.log'
 
+set :linked_files, []
+
+set :default_env, {
+  APP_ENV: 'prod'
+}
+
 after 'deploy:cleanup', 'linio:php:fpm:restart'

c71425b20011b9aa6bf23b7a006f6725369a53e8
Harrison Heck <harrison.heck@gmail.com>
2018-09-19 18:09:59 -0400

    ci: remove capistrano

--- deploy/environments/live.rb
+++
@@ -1,3 +0,0 @@
-set :new_relic_api_key, "ebd82358f565d8c5d1b1bd8c6729cc603deba760fcc4a91"
-
-aws_ec2_register user: 'www-data'
--- deploy/environments/staging01.rb
+++
@@ -1,4 +0,0 @@
-set :new_relic_api_key, "2dfb320c93c5013cded743e021ece238f4176041c347db"
-set :new_relic_app_name, "seller-signup-form01"
-
-server 'web01.linio-staging.com', user: 'www-data', roles: %w{app}

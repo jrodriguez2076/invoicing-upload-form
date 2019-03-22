9e1d7dc59a6a5f2718cf7a9b98a93baa5692ad8e
Pedro Molina <pmolina2602@gmail.com>
2019-03-22 12:22:26 -0600

    feat: add attachment fields

--- src/Form/Store/ar/AdditionalInfoType.php
+++ src/Form/Store/ar/AdditionalInfoType.php
@@ -7,6 +7,7 @@ namespace App\Form\Store\ar;
 use App\Service\ParameterService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,6 +29,280 @@ class AdditionalInfoType extends AbstractType
         $store = $options['store'];
 
         $builder
+            ->add(
+                'paymentDetails',
+                FileType::class,
+                [
+                    'label' => 'PAYMENT_DETAILS_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide paymentDetails files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide paymentDetails',
+                    ],
+                ]
+            )
+            ->add(
+                'taxDocument',
+                FileType::class,
+                [
+                    'label' => 'TAX_DOCUMENT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide taxDocument files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide taxDocument',
+                    ],
+                ]
+            )
+            ->add(
+                'companyDocument',
+                FileType::class,
+                [
+                    'label' => 'COMPANY_DOCUMENT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide companyDocument files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide companyDocument',
+                    ],
+                ]
+            )
+            ->add(
+                'accountBalance',
+                FileType::class,
+                [
+                    'label' => 'ACCOUNT_BALANCE_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide accountBalance files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide accountBalance',
+                    ],
+                ]
+            )
+            ->add(
+                'invoices',
+                FileType::class,
+                [
+                    'label' => 'INVOICES_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide invoices files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide invoices',
+                    ],
+                ]
+            )
+            ->add(
+                'accountReceivable',
+                FileType::class,
+                [
+                    'label' => 'ACCOUNT_RECEIVABLE_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide accountReceivable files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide accountReceivable',
+                    ],
+                ]
+            )
+            ->add(
+                'interfacturaEvidence',
+                FileType::class,
+                [
+                    'label' => 'INTERFACTURA_EVIDENCE_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide interfacturaEvidence files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide interfacturaEvidence',
+                    ],
+                ]
+            )
+            ->add(
+                'evidence',
+                FileType::class,
+                [
+                    'multiple' => true,
+                    'label' => 'EVIDENCE_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide evidence files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide evidence',
+                    ],
+                ]
+            )
+            ->add(
+                'fblSkuList',
+                FileType::class,
+                [
+                    'label' => 'FBL_SKU_LIST_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide fblSkuList files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide fblSkuList',
+                    ],
+                ]
+            )
+            ->add(
+                'skuList',
+                FileType::class,
+                [
+                    'label' => 'SKU_LIST_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide skuList files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide skuList',
+                    ],
+                ]
+            )
+            ->add(
+                'shoppingError',
+                FileType::class,
+                [
+                    'label' => 'SHOPPING_ERROR_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide shoppingError files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide shoppingError',
+                    ],
+                ]
+            )
+            ->add(
+                'eventsForm',
+                FileType::class,
+                [
+                    'label' => 'EVENTS_FORM_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide eventsForm files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide eventsForm',
+                    ],
+                ]
+            )
+            ->add(
+                'orderShipmentDocument',
+                FileType::class,
+                [
+                    'label' => 'ORDER_SHIPMENT_DOCUMENT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide orderShipmentDocument files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide orderShipmentDocument',
+                    ],
+                ]
+            )
+            ->add(
+                'cbuDocument',
+                FileType::class,
+                [
+                    'label' => 'CBU_DOCUMENT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide cbuDocument files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide cbuDocument',
+                    ],
+                ]
+            )
+            ->add(
+                'requestedSupplies',
+                FileType::class,
+                [
+                    'label' => 'REQUESTED_SUPPLIES_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide requestedSupplies files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide requestedSupplies',
+                    ],
+                ]
+            )
+            ->add(
+                'invoiceProof',
+                FileType::class,
+                [
+                    'label' => 'INVOICE_PROOF_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide invoiceProof files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide invoiceProof',
+                    ],
+                ]
+            )
+            ->add(
+                'alternateImage',
+                FileType::class,
+                [
+                    'label' => 'ALTERNATE_IMAGE_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide alternateImage files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide alternateImage',
+                    ],
+                ]
+            )
+            ->add(
+                'technicalReport',
+                FileType::class,
+                [
+                    'label' => 'TECHNICAL_REPORT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide technicalReport files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide technicalReport',
+                    ],
+                ]
+            )
+            ->add(
+                'ccciDocument',
+                FileType::class,
+                [
+                    'label' => 'CCCI_DOCUMENT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide ccciDocument files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide ccciDocument',
+                    ],
+                ]
+            )
+            ->add(
+                'idDocument',
+                FileType::class,
+                [
+                    'label' => 'ID_DOCUMENT_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide idDocument files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide idDocument',
+                    ],
+                ]
+            )
+            ->add(
+                'shippingLabel',
+                FileType::class,
+                [
+                    'label' => 'SHIPPING_LABEL_LABEL',
+                    'attr' => [
+                        'class' => 'additionalField hide shippingLabel files',
+                    ],
+                    'label_attr' => [
+                        'class' => 'additionalField hide shippingLabel',
+                    ],
+                ]
+            )
             ->add(
                 'cutoffDates',
                 TextType::class,

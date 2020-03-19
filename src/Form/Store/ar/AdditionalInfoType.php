e5b26d3bee99c8a57122fe2f06124f40266f23d8
Pedro Molina <pmolina2602@gmail.com>
2020-03-19 12:27:47 -0600

    feat: change reason field to text area type

--- src/Form/Store/ar/AdditionalInfoType.php
+++ src/Form/Store/ar/AdditionalInfoType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -418,7 +419,7 @@ class AdditionalInfoType extends AbstractType
             )
             ->add(
                 'reason',
-                TextType::class,
+                TextareaType::class,
                 [
                     'label' => 'REASON_LABEL',
                     'attr' => [
